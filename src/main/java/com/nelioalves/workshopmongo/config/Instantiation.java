3b58b4994f3eabb67b811a470dad6dbea3d7633b
LeoDalmazo <leo.dalmazo@hotmail.com>
2022-07-03 20:48:14 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/nelioalves/workshopmongo/config/Instantiation.java
+++ src/main/java/com/nelioalves/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.nelioalves.workshopmongo.domain.Post;
 import com.nelioalves.workshopmongo.domain.User;
+import com.nelioalves.workshopmongo.dto.AuthorDTO;
 import com.nelioalves.workshopmongo.repository.PostRepository;
 import com.nelioalves.workshopmongo.repository.UserRepository;
 
@@ -18,13 +19,13 @@ public class Instantiation implements CommandLineRunner {
 
 	@Autowired
 	private UserRepository userReposiroty;
-	
+
 	@Autowired
 	private PostRepository postReposiroty;
-	
+
 	@Override
 	public void run(String... arg0) throws Exception {
-		
+
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 
@@ -34,10 +35,11 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
-
 		userReposiroty.saveAll(Arrays.asList(maria, alex, bob));
+
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
 		postReposiroty.saveAll(Arrays.asList(post1, post2));
 	}
 
